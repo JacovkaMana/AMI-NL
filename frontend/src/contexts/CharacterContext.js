52f85361720f2e26922ac1408b32307b20a63231
JacovkaMana <yasha.protasov@gmail.com>
2024-11-26 01:04:46 +0300

    sprint 8 chat addons

--- frontend/src/contexts/CharacterContext.js
+++ frontend/src/contexts/CharacterContext.js
@@ -1,1 +1,29 @@
- 
+import { createContext, useContext, useState } from 'react';
+
+const CharacterContext = createContext();
+
+export function CharacterProvider({ children }) {
+  const [characters, setCharacters] = useState([]);
+  const [selectedCharacter, setSelectedCharacter] = useState(null);
+
+  const value = {
+    characters,
+    setCharacters,
+    selectedCharacter,
+    setSelectedCharacter,
+  };
+
+  return (
+    <CharacterContext.Provider value={value}>
+      {children}
+    </CharacterContext.Provider>
+  );
+}
+
+export function useCharacter() {
+  const context = useContext(CharacterContext);
+  if (context === undefined) {
+    throw new Error('useCharacter must be used within a CharacterProvider');
+  }
+  return context;
+} 
--- frontend/src/pages/_app.jsx
+++ frontend/src/pages/_app.jsx
@@ -1,14 +1,17 @@
-import { ThemeProvider } from '../contexts/ThemeContext';
 import { AuthProvider } from '../contexts/AuthContext';
+import { ThemeProvider } from '../contexts/ThemeContext';
+import { CharacterProvider } from '../contexts/CharacterContext';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider>
-      <AuthProvider>
-        <Component {...pageProps} />
-      </AuthProvider>
-    </ThemeProvider>
+    <AuthProvider>
+      <ThemeProvider>
+        <CharacterProvider>
+          <Component {...pageProps} />
+        </CharacterProvider>
+      </ThemeProvider>
+    </AuthProvider>
   );
 }
 
